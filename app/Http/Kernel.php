4bbdfb0d22d50f166cc39859e804634ede6aa164
tranvanthinh <xuanloc23082000@gmail.com>
2023-07-18 11:16:17 +0700

    middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'checkLogic' => \App\Http\Middleware\CheckAge::class,
+        'verified.admin' => \App\Http\Middleware\VerifyAdminMiddleware::class,
     ];
 }
